0313494f8d0cff75389d5661fb6f2cd68f7e0fae
Alex Hanson <alexhanson@users.noreply.github.com>
2015-11-16 14:58:12 -0800

    Create a basic `rdio-export` script that, presently, just shows the user their own profile data.

--- rdioexport/__init__.py
+++ rdioexport/__init__.py
@@ -1,1 +1,21 @@
-IT_WORKS = True
+from collections import namedtuple
+from rdioapi import Rdio
+
+RdioCredentials = namedtuple('RdioCredentials', [
+	'client_id',
+	'client_secret',
+])
+
+def get_credentials_from_input():
+	client_id = raw_input("Client ID: ")
+	client_secret = raw_input("Client secret: ")
+	return RdioCredentials(
+		client_id=client_id,
+		client_secret=client_secret)
+
+def get_rdio_client(credentials):
+	state = {}
+	return Rdio(
+		credentials.client_id,
+		credentials.client_secret,
+		state)
