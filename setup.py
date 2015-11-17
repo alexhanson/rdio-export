90d22ee5626bee0bf2ad8c75fca0a7c1f1f3701f
Alex Hanson <alexhanson@users.noreply.github.com>
2015-11-17 01:03:19 -0800

    A couple important fields to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from setuptools import setup
 setup(
     name='rdio-export',
     version='0.0.1',
+    url='https://github.com/alexhanson/rdio-export',
+    license="ISC License",
     install_requires=[
         'Rdio==0.3.0',
     ],
